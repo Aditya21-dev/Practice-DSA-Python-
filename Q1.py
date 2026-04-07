1962238a886363923ed392f2d06f408fa38bdb46
Aditya21-dev <adityadas0217@gmail.com>
2026-04-07 14:55:33 +0530

    Question 1

---
+++ Q1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}  # value -> index
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in lookup:
+                return [lookup[complement], i]
+
+            lookup[nums[i]] = i
